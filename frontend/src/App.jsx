fd1d6fd2a0cf5ad61c44501f912549d30e523452
priyanshi-717 <prinshupatel001@gmail.com>
2026-04-15 23:14:59 +0530

    basic ui done and its working in react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className="app">
+
+  
+      <div className="header">
+        <h1>Samadhan with AI</h1>
+        <p>Rural Intelligence System</p>
+      </div>
+
+     
+      <div className="main">
+        <button className="primary-btn">Start Speaking</button>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
